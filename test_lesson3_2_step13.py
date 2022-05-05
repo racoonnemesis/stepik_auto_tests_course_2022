113884f28650f4eb893ee4805941b1634af1df28
SShakirov <sshakirov@luxoft.com>
2022-05-05 17:09:47 +0200

    new tests

--- test_lesson3_2_step13.py
+++ test_lesson3_2_step13.py
@@ -5,11 +5,10 @@ import time
 
 class TestPage(unittest.TestCase):
     def test_page1(self):
-        link = "http://suninjuly.github.io/registration2.html"
+        link = "http://suninjuly.github.io/registration1.html"
         browser = webdriver.Chrome()
         browser.get(link)
 
-        # Ваш код, который заполняет обязательные поля
         input1 = browser.find_element_by_css_selector(".first_block .form-control.first")
         input1.send_keys("Ivan")
         input2 = browser.find_element_by_css_selector(".first_block .form-control.second")
@@ -17,25 +16,42 @@ class TestPage(unittest.TestCase):
         input3 = browser.find_element_by_css_selector(".first_block .form-control.third")
         input3.send_keys("xxx@xxx.com")
 
-
-        # Отправляем заполненную форму
         button = browser.find_element_by_css_selector("button")
         button.click()
 
-        # Проверяем, что смогли зарегистрироваться
-        # ждем загрузки страницы
         time.sleep(1)
 
-        # находим элемент, содержащий текст
         welcome_text_elt = browser.find_element_by_tag_name("h1")
-        # записываем в переменную welcome_text текст из элемента welcome_text_elt
         welcome_text = welcome_text_elt.text
 
         self.assertEqual(welcome_text,"Congratulations! You have successfully registered!",'Wrong message!!!')
 
-        # ожидание чтобы визуально оценить результаты прохождения скрипта
         time.sleep(10)
-        # закрываем браузер после всех манипуляций
+        browser.quit()
+
+    def test_page2(self):
+        link = "http://suninjuly.github.io/registration2.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        input1 = browser.find_element_by_css_selector(".first_block .form-control.first")
+        input1.send_keys("Ivan")
+        input2 = browser.find_element_by_css_selector(".first_block .form-control.second")
+        input2.send_keys("Petrov")
+        input3 = browser.find_element_by_css_selector(".first_block .form-control.third")
+        input3.send_keys("xxx@xxx.com")
+
+        button = browser.find_element_by_css_selector("button")
+        button.click()
+
+        time.sleep(1)
+
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+        welcome_text = welcome_text_elt.text
+
+        self.assertEqual(welcome_text,"Congratulations! You have successfully registered!",'Wrong message!!!')
+
+        time.sleep(10)
         browser.quit()
 
 if __name__ == "__main__":
